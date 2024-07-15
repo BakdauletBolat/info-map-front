36991fd826dccfd9d808badf2da2de0719a54949
Bakdaulet <bakosh21345@gmail.com>
2024-07-16 00:21:55 +0500

    added photo

--- src/components/GeographicInfoComponent.vue
+++ src/components/GeographicInfoComponent.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import {IGeographicRegion} from "@/domain/models.ts";
 import {geographic_region, showInfo} from "@/domain/stores.ts";
-import {HomeModernIcon, UserGroupIcon, ArrowUpOnSquareStackIcon, ExclamationCircleIcon} from "@heroicons/vue/24/outline";
+import {HomeModernIcon, UserGroupIcon, ExclamationCircleIcon} from "@heroicons/vue/24/outline";
 import {onChangeCity} from "@/domain/map-store.ts";
 import {useRouter} from "vue-router";
 
