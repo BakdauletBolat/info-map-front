ffc9bc7776b8f5426755ff7697fcfdfa93961a3f
Bakdaulet <bakosh21345@gmail.com>
2024-07-16 00:20:54 +0500

    added photo

--- src/components/InfoMapViewComponents/InfoTabComponent.vue
+++ src/components/InfoMapViewComponents/InfoTabComponent.vue
@@ -2,6 +2,7 @@
 import {geographic_region} from "@/domain/stores.ts";
 import {ref} from "vue";
 import GeographicInfoComponent from "@/components/GeographicInfoComponent.vue";
+import ImageComponent from "@/components/ImageComponent.vue";
 
 const activeTab = ref<number>(0);
 
@@ -18,11 +19,14 @@ const activeTab = ref<number>(0);
         }">Информация о место</div>
     </div>
     <GeographicInfoComponent v-if="activeTab==0" :region="geographic_region!"></GeographicInfoComponent>
-    <div v-if="activeTab==1">
-      <h2 class="font-bold">Название</h2>
-      <p class="text-sm">{{geographic_region?.name}}</p>
-      <p class="mt-2">Описание</p>
-      <p class="text-sm">{{geographic_region?.description}}</p>
+    <div class="grid mt-4 gap-2 lg:grid-cols-[1fr_350px]" v-if="activeTab==1">
+      <div>
+        <h2 class="font-bold">Название</h2>
+        <p class="text-sm">{{geographic_region?.name}}</p>
+        <p class="mt-2 font-bold">Описание</p>
+        <p class="text-sm">{{geographic_region?.description}}</p>
+      </div>
+      <ImageComponent class="rounded" :alt="geographic_region?.name" :url="geographic_region?.photo"/>
     </div>
   </div>
 </template>
